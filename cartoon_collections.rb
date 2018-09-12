f3b0e4e1fa7ce5921188ceae39bf0ebe46c3bd72
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-09-12 18:03:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index do |value, index|
+    order = index + 1
+    puts "#{order}. #{value}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect do |call|
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+  check = []
+  while i < array.length
+    array.each {|item| check << cheese_types.include?(item)}
+    array[i] if check[i] == true
+  binding.pry
+    i += 1
+  end
 end
